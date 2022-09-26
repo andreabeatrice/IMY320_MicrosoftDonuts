fa5475dc12e505bc8aa73b207f1b5562be06d23c
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-26 12:30:27 +0200

    attempt2

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//CREATE APP
+var app = (0, _express["default"])(); //SERVE A STATIC PAGE IN THE PUBLIC DIRECTORY
+
+app.use(_express["default"]["static"]("public")); //PORT TO LISTEN TO
+
+app.listen(8080, function () {
+  console.log("Listening on localhost:8080");
+});
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+ 
+//CREATE APP
+const app = express();
+ 
+//SERVE A STATIC PAGE IN THE PUBLIC DIRECTORY
+app.use(express.static("public"));
+ 
+//PORT TO LISTEN TO
+app.listen(8080, () => {
+    console.log("Listening on localhost:8080");
+});
