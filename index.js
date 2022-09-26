4d674df8032760c9f5b31d891bb437e6e03ad791
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-26 13:07:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile(path.resolve('./public/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
