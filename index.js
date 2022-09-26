eff54a69975b68385d24a3d614976fb7db76eb55
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-26 13:37:05 +0200

    donut

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.sendFile(path.resolve('./public/index.html')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
